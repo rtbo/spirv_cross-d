83938d72c49305c2b3967a905baa6844923b9ebe
Remi Thebault <remi.thebault@gmail.com>
2018-03-25 11:57:33 +0200

    abort D build if c++ build fails

--- scripts/build_cpp.sh
+++ scripts/build_cpp.sh
@@ -17,10 +17,10 @@ PACKAGE_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/..
 CPP_DIR=$PACKAGE_DIR/cpp
 BUILD_DIR=$PACKAGE_DIR/.dub/build/sc_cpp_$BUILD_TYPE
 
-mkdir -p $BUILD_DIR
+mkdir -p $BUILD_DIR || exit 1
 cd $BUILD_DIR
 
-cmake -G $GEN -DCMAKE_BUILD_TYPE=$BUILD_TYPE $CPP_DIR
-cmake --build $BUILD_DIR
+cmake -G $GEN -DCMAKE_BUILD_TYPE=$BUILD_TYPE $CPP_DIR || exit 1
+cmake --build $BUILD_DIR || exit 1
 
-cp $BUILD_DIR/libspirv_cross_cpp.a $PACKAGE_DIR
+cp $BUILD_DIR/libspirv_cross_cpp.a $PACKAGE_DIR || exit 1
